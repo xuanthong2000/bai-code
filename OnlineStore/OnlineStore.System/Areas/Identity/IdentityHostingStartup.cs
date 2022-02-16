4cf41231328c772fabae45638dc77c58675f3ad4
xuanthong2000 <56944271+xuanthong2000@users.noreply.github.com>
2022-02-16 10:36:52 +0700

    code

---
+++ OnlineStore/OnlineStore.System/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineStore.System.Areas.Identity.Data;
+using OnlineStore.System.Data;
+
+[assembly: HostingStartup(typeof(OnlineStore.System.Areas.Identity.IdentityHostingStartup))]
+namespace OnlineStore.System.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<OnlineStoreDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("OnlineStoreDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    })
+                    .AddEntityFrameworkStores<OnlineStoreDbContext>();
+            });
+        }
+    }
+}
